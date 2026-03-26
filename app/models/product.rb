5ca26d55a1bb0adf0a71389a4857b2fbbe16f15f
louis17782 <luissanteliz22@gmail.com>
2026-03-26 11:34:59 -0400

    delete validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,6 @@ class Product < ApplicationRecord
   has_many_attached :slider_images
 
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
-  validates :price_usd, numericality: { greater_than_or_equal_to: 0 }
-  validates :price_bs, numericality: { greater_than_or_equal_to: 0 }
   validates :name, :description, :quantity, :image, :sale_type, presence: true
   scope :with_slider_images, -> {
       joins(:slider_images_attachments)
